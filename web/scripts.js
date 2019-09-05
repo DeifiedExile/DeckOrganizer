7b563e59b6a0887a180760a09c911f0f430380e5
Lucas <lwolfs@my.wctc.edu>
2019-09-04 19:44:09 -0500

    New deck form working using session storage

--- web/scripts.js
+++ web/scripts.js
@@ -7,6 +7,10 @@ $(document).ready(function(){
             url: ('DeckData.csv'),
             dataType:"text",
             success:function(data){
+                if("newdeck" in sessionStorage)
+                {
+                    data += sessionStorage.getItem("newdeck");
+                }
                 var cards = $.csv.toObjects(data);
 
 
@@ -50,6 +54,45 @@ $(document).ready(function(){
             sessionStorage.setItem('DeckName', this.text);
             console.log($(this).text);
         });
+
+        //add new card entry form line
+    $(document).on('click', '#addcard', function(e){
+        e.preventDefault();
+        $(".cardlistentry").last().after("<tr class=\"form-group cardlistentry\">\n" +
+            "                    <td>\n" +
+            "                        <div class=\"col-sm-6 offset-sm-2\"><input type=\"number\" class=\"form-control input-sm cardentryqty\" ></div>\n" +
+            "                    </td>\n" +
+            "                    <td>\n" +
+            "                        <div class=\"col-sm-12 \"><input type=\"text\" class=\"form-control input-med cardentryname\" ></div>\n" +
+            "                    </td>\n" +
+            "                </tr>");
+    });
+
+    //submit new deck
+    $('input:submit').on('click', function(e){
+
+        var cardcount = 0;
+        if( typeof deckcsv === 'undefined' || deckcsv === null) {
+            var deckcsv = "";
+        }
+        else
+        {
+            var deckcsv = sessionStorage.getItem("newdeck");
+        }
+
+        $('.cardlistentry').each(function(i,obj){
+            deckcsv += 'Commander,';
+            deckcsv += $('#deckName').val();
+            deckcsv += ',' + parseInt($(this).find('.cardentryqty').val()) + ',' + $(this).find('.cardentryname').val() + '\n';
+
+            cardcount += parseInt($(this).find('.cardentryqty').val());
+        })
+        sessionStorage.setItem('cardcount', cardcount.toString());
+        sessionStorage.setItem('newdeckname', $('#deckName').val());
+        sessionStorage.setItem('newdeck', deckcsv);
+        $('a #decksubmit').click();
+
+    });
 
 });
 
